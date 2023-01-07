a436265e1955f7497ff13257cf65c74abc19a457
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-01-06 18:14:03 -0700

    finish optionsBar

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import logging as logging
+from datetime import datetime
+
 import os
 
 import tkinter as tk
@@ -8,17 +11,38 @@ import tkinter.colorchooser as colorchooser
 import PIL as pil
 import json
 
+def createLogger(type = 'console'):
+    format = '%(asctime)s %(levelname)s:%(message)s'
+    datefmt = '%I:%M:%S %p'
+    level = logging.DEBUG
+
+    filename = f'logs/{datetime.now().strftime("%m-%d-%y_%H-%M-%S")}.log'
+
+    if type == 'file':
+        try:
+            os.mkdir('logs')
+        except:
+            pass
+
+        logging.basicConfig(filename=filename, filemode='w', format=format, datefmt=datefmt, level=level)
+    else:
+        logging.basicConfig(format=format, datefmt=datefmt, level=level)
+
+createLogger()
+# createLogger('file')
+
 class Window(tk.Tk):
     def __init__(this, *args, **kwargs):
         super().__init__(*args, **kwargs)
         this.title('')
         this.geometry('%dx%d' % (760 , 610) )
+        this.minsize(700, 300)
 
         this.panedwindow = ttk.PanedWindow(orient='horizontal')
         this.panedwindow.pack(fill='both', expand=True)
 
         this.optionsframe = ttk.LabelFrame(this.panedwindow, text='Options', width=250)
-        this.mainframe = ttk.Frame(this.panedwindow, borderwidth=2, width=500)
+        this.mainframe = ttk.Frame(this.panedwindow, borderwidth=2)
 
         this.panedwindow.add(this.optionsframe)
         this.panedwindow.add(this.mainframe)
@@ -32,6 +56,13 @@ class Window(tk.Tk):
 
         this.initialize()
 
+    def initialize(this):
+        this.isnumber = (this.register(this.checkNumber), '%S', '%d')
+        this.isint = this.register(lambda *args: this.checkNumber(*args, exclude=['-', '.']))
+        this.createCanvas()
+        this.createOptionsBar()
+        this.createOptionsTab()
+    
     def createMenuBar(this):
         this.menuBar = tk.Menu(this)
         this.config(menu=this.menuBar)
@@ -44,12 +75,6 @@ class Window(tk.Tk):
         this.fileMenu.add_command(label='Save as...')
 
         this.menuBar.add_cascade(label= 'File', menu=this.fileMenu)
-
-
-    def initialize(this):
-        this.isnumber = (this.register(this.checkNumber), '%5', '%d')
-        this.createCanvas()
-        this.createOptionsBar()
 
     def createCanvas(this):
         this.canvas = tk.Canvas(this.mainframe, background='white')
@@ -87,22 +112,27 @@ class Window(tk.Tk):
         maxY = max([this.size[1]] + ylist)
         this.canvas.config(scrollregion=(minX - 200, minY - 200, maxX + 200, maxY + 200))
 
-    def checkNumber(this, value = None, action = None):
+    def createOptionsTab(this):
+        # entry = ttk.Entry(this.optionsframe, validate='all', validatecommand=lambda args: print(args))
+        # entry.pack()
+        pass
+
+    def checkNumber(this, value = None, action = None, include = [], exclude = [], callback = None):
             """
-            value = %5
+            value = %S
             action = %d
             """
 
-            print(value, action)
+            logging.info(f'{value}, {action}')
 
-            result = False
+            # result = False
 
             # if int(action) < 1 and action != None:
             #     result = True
 
-            result = value in ['-', '.'] or str(value).isnumeric()
+            result = ((value in ['-', '.'] or value in include) or str(value).isnumeric()) and not value in exclude
 
-            print(result)
+            logging.info(result)
             return result
     
     
@@ -110,17 +140,66 @@ class Window(tk.Tk):
     def createOptionsBar(this):
 
         this.optionsBar = ttk.Frame(this.mainframe)
+
+        entryWidth = 5
         
         this.optionsWidgets = {
             'x_spacing': {
                 'label': ttk.Label(this.optionsBar, text='X Spacing'),
-                'spinbox': ttk.Spinbox(this.optionsBar, width=10, validate='key', validatecommand=this.isnumber)
-            }
+                'var': tk.IntVar(),
+            },
+            'y_spacing': {
+                'label': ttk.Label(this.optionsBar, text='Y Spacing'),
+                'var': tk.IntVar(),
+            },
+            'width': {
+                'label': ttk.Label(this.optionsBar, text='Sheet Width'),
+                'var': tk.IntVar(),
+            },
+            'auto_width': {
+                'var': tk.BooleanVar(),
+            },
         }
+
+        this.optionsWidgets['x_spacing']['spinbox'] = ttk.Spinbox(this.optionsBar, textvariable=this.optionsWidgets['x_spacing']['var'], width=entryWidth, validate='key', validatecommand=(this.isint, '%S', '%d'), from_=0, to=999)
+        this.optionsWidgets['y_spacing']['spinbox'] = ttk.Spinbox(this.optionsBar, textvariable=this.optionsWidgets['y_spacing']['var'], width=entryWidth, validate='key', validatecommand=(this.isint, '%S', '%d'), from_=0, to=999)
+        this.optionsWidgets['width']['spinbox'] = ttk.Spinbox(this.optionsBar, textvariable=this.optionsWidgets['width']['var'], width=entryWidth, validate='key', validatecommand=(this.isint, '%S', '%d'), from_=0, to=999)
+        this.optionsWidgets['auto_width']['checkbox'] = ttk.Checkbutton(this.optionsBar, text='Minimize Width', variable=this.optionsWidgets['auto_width']['var'], onvalue=True, offvalue=False, command=this.updateSettings)
+
+        this.optionsWidgets['x_spacing']['var'].set(this.settings['x_spacing'])
+        this.optionsWidgets['y_spacing']['var'].set(this.settings['y_spacing'])
+        this.optionsWidgets['width']['var'].set(this.settings['width'])
+        this.optionsWidgets['auto_width']['var'].set(int(this.settings['auto_width']))
+        
+        this.optionsWidgets['x_spacing']['var'].trace("w", lambda name, index, mode: this.updateSettings() )
+        this.optionsWidgets['y_spacing']['var'].trace("w", lambda name, index, mode: this.updateSettings() )
+        this.optionsWidgets['width']['var'].trace("w", lambda name, index, mode: this.updateSettings() )
+        this.optionsWidgets['auto_width']['var'].trace("w", lambda name, index, mode: this.updateSettings() )
+
+        this.optionsWidgets['auto_width']['checkbox'].pack(side='right', padx=2)
+
+        this.optionsWidgets['width']['spinbox'].pack(side='right', padx=2)
+        this.optionsWidgets['width']['label'].pack(side='right', padx=2)
+
+        this.optionsWidgets['y_spacing']['spinbox'].pack(side='right', padx=2)
+        this.optionsWidgets['y_spacing']['label'].pack(side='right', padx=2)
+
         this.optionsWidgets['x_spacing']['spinbox'].pack(side='right', padx=2)
         this.optionsWidgets['x_spacing']['label'].pack(side='right', padx=2)
 
-        this.optionsBar.pack(side='bottom', fill='x')
+
+
+        this.optionsBar.pack( fill='x')
+
+    def updateSettings(this):
+        this.settings['x_spacing'] = this.optionsWidgets['x_spacing']['var'].get()
+        this.settings['y_spacing'] = this.optionsWidgets['y_spacing']['var'].get()
+        this.settings['width'] =this.optionsWidgets['width']['var'].get()
+        this.settings['auto_width'] = this.optionsWidgets['auto_width']['var'].get()
+
+        logging.info(this.settings)
+
+        this.saveSettings()
 
     # settings
     def loadSettings(this, **kwargs):
@@ -131,7 +210,7 @@ class Window(tk.Tk):
                 with open(this.settingsFile, 'r') as file:
                     this.settings = json.load(file)
 
-                print(this.settings)
+                logging.info(this.settings)
             except:
                 this.initSettings()
                 this.saveSettings()
